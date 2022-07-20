33cf5d71595fcbf2fbd702d3b145c57b2f957824
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2022-07-20 16:17:21 -0600

    separated files

---
+++ SimpleSprite.lua
@@ -0,0 +1,59 @@
+---@class SimpleSprite
+SimpleSprite = {
+  _isShowing = true,
+  _imgTimer = 0,
+  _shouldHide = false,
+  _texture = nil,
+
+  new = function(self, imgName)
+    local o = {imgName = imgName}
+    self.__index = self
+    self._texture = Hyperspace.Resources:GetImageId(imgName .. ".png")
+    return setmetatable(o, self)
+  end,
+
+  _processModifiers = function(self, modifierTable)
+    local width = modifierTable.width or self._texture.width
+    local height = modifierTable.height or self._texture.height
+    local positionX = 1280 / 2 - width / 2 + (modifierTable.Xalign or 0)
+    local positionY = 720 / 2 - height / 2 - (modifierTable.Yalign or 0)
+    local rotation = modifierTable.rotation or 0
+    local color = modifierTable.color or Graphics.GL_Color(1, 1, 1, 1)
+    local isMirror = modifierTable.isMirror or false
+    return {positionX, positionY, width, height, rotation, color, isMirror}
+  end,
+
+  show = function(self, modifierTable)
+    
+    if not self._isShowing then
+      return
+    end
+    
+    modifierTable = modifierTable or {}
+    local processedModifiers = self:_processModifiers(modifierTable)
+    Graphics.CSurface.GL_BlitImage(
+      self._texture,
+      table.unpack(processedModifiers)
+    )
+  end,
+  hide = function(self)
+    local isTimerInitialized = self._imgTimer ~= 0
+    if self._shouldHide or (not isTimerInitialized) then
+      self._isShowing = false
+      self._shouldHide = false
+    end
+  end,
+  wait = function(self, sec)
+    if self._shouldHide then return end
+
+    self._imgTimer = self._imgTimer + (Hyperspace.FPS.SpeedFactor / 16)
+    if self._imgTimer > sec then
+      self._shouldHide = true
+    end
+  end,
+  reset = function(self)
+    self._imgTimer = 0
+    self._isShowing = true
+    self._shouldHide = false
+  end,
+}
---
+++ simple_tests.lua
@@ -0,0 +1,36 @@
+local bacon = SimpleSprite:new("bacon")
+local bacon2 = SimpleSprite:new("bacon")
+local x = -250
+local y = 0
+local sqrt = math.sqrt
+local bool = false
+
+script.on_game_event("TEST_LUA", false, function()
+  bool = not bool
+end)
+
+script.on_render_event(Defines.RenderEvents.LAYER_PLAYER, 
+  function() end, 
+  function()
+
+    if not bool then 
+      bacon:reset()
+      bacon2:reset()
+      x = -250
+      y = 0
+      return 
+    end
+
+    bacon:show({Xalign = x, Yalign = y})
+    bacon2:show({Xalign = x, Yalign = -y})
+
+    if x > 250 then
+      bacon:hide()
+      bacon2:hide()
+    else
+      x = x + 1
+      y = sqrt(250 ^ 2 - x ^ 2)
+    end
+
+  end
+)
